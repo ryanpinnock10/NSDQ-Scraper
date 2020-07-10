269949ea76a2457fe26cd4b31e56f0a2ec34befa
Ryan Pinnock <ryanpinnock10@gmail.com>
2020-07-10 16:19:32 -0400

    Resturcturing happened

--- news.py
+++ news.py
@@ -1,3 +1,17 @@
-import requests, csv
-from bs4 import BeautifulSoup
-import pandas as pd 
+#Pandas - Data analysis library used to create and manipulate data
+# within structures called dataframes
+
+#Requests - An HTTP library for Python with a built-in JSON decoder
+
+#Datetime - Functions to create & format date and time objects in Python
+
+#Calendar - Contains a method to retrieve the # of days in a month given a year
+
+import requests, pandas, datetime, calendar
+
+activeUrl = https://www.nasdaq.com/market-activity/stocks/screener
+req = requests.get(activeUrl)
+data = req.text
+soup = BeautifulSoup(data)
+
+table = soup.find_all('div', attrs={&quot})
